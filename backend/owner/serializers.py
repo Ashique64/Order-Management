01e20997506f8c10cee615366fc5bee9abfa07eb
Ashique <ashique9496@gmail.com>
2025-02-13 13:18:59 +0530

    Restaurant listing completed

--- backend/owner/serializers.py
+++ backend/owner/serializers.py
@@ -27,21 +27,47 @@ class LoginSerializer(serializers.Serializer):
 
     def validate(self, data):
         email = data.get('email')
-        print(email)
         restaurant_id = data.get('restaurant_id')
-
-        # try:
-        #     email = Owner.objects.get(email=email) 
-        # except Owner.DoesNotExist:
-        #     raise serializers.ValidationError("Owner not found")
-
-        # data['email'] = email
 
         try:
             restaurant = Restaurant.objects.get(pk=restaurant_id)
         except Restaurant.DoesNotExist:
             raise serializers.ValidationError("Restaurant not found")
+        
+        
+        owner_exists = Owner.objects.filter(email=email).exists()
+        staff_exists = Staff.objects.filter(email=email, restaurant=restaurant).exists()
+        
+        if not (owner_exists or staff_exists):
+            raise serializers.ValidationError("Invalid email for this restaurant")
 
         data['restaurant'] = restaurant
         
         return data
+    
+class StaffCreateSerializer(serializers.ModelSerializer):
+    owner_email = serializers.EmailField(write_only=True)
+    restaurant_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Staff
+        fields = ['name', 'email', 'restaurant_id', 'owner_email']
+
+    def create(self, validated_data):
+        # Remove owner_email as it's not part of the Staff model
+        owner_email = validated_data.pop('owner_email')
+        restaurant_id = validated_data.pop('restaurant_id')
+        
+        try:
+            restaurant = Restaurant.objects.get(
+                restaurant_id=restaurant_id,
+                owner__email=owner_email
+            )
+            return Staff.objects.create(restaurant=restaurant, **validated_data)
+        except Restaurant.DoesNotExist:
+            raise serializers.ValidationError("Invalid restaurant or owner")
+    
+    
+
+
+
--- backend/owner/urls.py
+++ backend/owner/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView,OwnerRestaurantsView,RestaurantStaffView,AddStaffView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('restaurants/', OwnerRestaurantsView.as_view(), name='owner-restaurants'),
+    path('restaurant/<int:restaurant_id>/staff/', RestaurantStaffView.as_view(), name='restaurant-staff'),
+    path('staff/add/', AddStaffView.as_view(), name='add-staff'),
 ]
